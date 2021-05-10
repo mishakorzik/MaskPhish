7e1c07ccda3e5692b11641620940ad2bf3eddd37
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-10 22:49:38 +0300

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,17 @@
+clear
+sleep 1
+echo    "|-----------[base installing]-----------|"
+sleep 1
+ORANGE="$(printf '\033[33m')" ${ORANGE}
+ORANGE="$(printf '\033[33m')" ${ORANGE}
+echo "|----------[Developer: mishakorzhik]----------|"
+sleep 2
+pkg update
+pkg upgrade
+pkg install curl
+pkg install php
+pkg install toilet
+sleep 1
+echo Installed has succesfull downloaded!
+sleep 1
+toilet -F gay Done
